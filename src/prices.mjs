31aa4cdae5ca2310a5e0ddb2fd65fe0e4a9bf9dd
jpasikainen <asikainen.juhapekka@gmail.com>
2022-02-28 10:12:34 +0200

    refactor

--- src/prices.mjs
+++ src/prices.mjs
@@ -87,11 +87,11 @@ function createApp(database) {
     return date.dayOfWeek === 1;
   }
 
-  function isHoliday(date2) {
+  function isHoliday(date) {
     const holidays = database.getHolidays();
     for (let row of holidays) {
-      let holiday2 = Temporal.PlainDate.from(row.holiday);
-      if (date2 && date2.equals(holiday2)) {
+      let holiday = Temporal.PlainDate.from(row.holiday);
+      if (date && date.equals(holiday)) {
         return true;
       }
     }
