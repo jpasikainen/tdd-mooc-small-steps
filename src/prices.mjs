75b811d7875e7db8051d7bb4ed50b4bda0e7083f
jpasikainen <asikainen.juhapekka@gmail.com>
2022-02-28 10:11:31 +0200

    refactor

--- src/prices.mjs
+++ src/prices.mjs
@@ -59,7 +59,7 @@ function createApp(database) {
   }
 
   function calculateCostForDayTicket(age, date, baseCost, date2) {
-    let reduction = calculateReduction(date, date2);
+    let reduction = calculateReduction(date2);
     if (age === undefined) {
       return Math.ceil(baseCost * (1 - reduction / 100));
     }
@@ -75,9 +75,9 @@ function createApp(database) {
     return Math.ceil(baseCost * (1 - reduction / 100));
   }
 
-  function calculateReduction(date, date2) {
+  function calculateReduction(date2) {
     let reduction = 0;
-    if (date && isMonday(date2) && !isHoliday(date2)) {
+    if (date2 && isMonday(date2) && !isHoliday(date2)) {
       reduction = 35;
     }
     return reduction;
